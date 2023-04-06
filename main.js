cf0e4a348654c2e892bac683eb9e718e96ea9120
Deeshan M <runner3222@gmail.com>
2023-04-06 09:08:52 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -6,3 +6,48 @@ https://youtu.be/hdI2bqOjy3c?t=4228
 
 Start at 1:10:29
  */
+
+console.log(document.getElementById('my-form'));
+
+// Newer method:
+
+console.log(document.querySelector('.container'));
+
+const items = document.querySelectorAll('.item');
+
+// items.forEach((item) => console.log(item));
+
+// Modifying:
+
+const ul = document.querySelector('.items');
+
+ul.firstElementChild.textContent = 'Hello World!';
+ul.children[1].innerHTML = '<strong>Brad</strong>';
+
+// CSS:
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'hotpink';
+
+// document.querySelector('body').classList.add('bg-dark');
+
+// Getting results:
+
+const myForm = document.querySelector('#my-form');
+const msg = document.querySelector('.msg');
+const nameInput = document.querySelector('#name');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+
+    e.preventDefault();
+
+    if(nameInput.value === '') {
+
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter a name.';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else console.log('Name entered.');
+}
